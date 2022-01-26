3c35929f1a9243064aebec9aea90687f479664ab
Claire Potter <clairepotter019@gmail.com>
2022-01-26 11:28:32 +0000

    Customise admin for store categories and products

--- store/models.py
+++ store/models.py
@@ -2,8 +2,12 @@ from django.db import models
 
 
 class Category(models.Model):
+
     name = models.CharField(max_length=254)
     call_name = models.CharField(max_length=254, null=True, blank=True)
+
+    class Meta:
+        verbose_name_plural = 'Categories'
 
     def __str__(self):
         return '%s' % (self.name)
