c31a4f99f56926bc04d64693e74ce007a7a94c4d
Claire Potter <clairepotter019@gmail.com>
2022-01-27 17:16:04 +0000

    Add sort options to sort by category

--- store/views.py
+++ store/views.py
@@ -4,14 +4,31 @@ from .models import Product, Category
 
 def music_store(request):
     """ A view to show the music store """
-    
+
     return render(request, 'music_store.html')
+
 
 def all_products(request):
     """ A view to show all products, including sorting and search queries """
 
     products = Product.objects.all()
     categories = None
+    sort = None
+    direction = None
+
+    if request.GET:
+        if 'sort' in request.GET:
+            sortkey = request.GET['sort']
+            sort = sortkey
+            if sortkey == 'name':
+                sortkey = 'lower_name'
+                products = products.annotate(lower_name=Lower('name'))
+
+            if 'direction' in request.GET:
+                direction = request.GET['direction']
+                if direction == 'desc':
+                    sortkey = f'-{sortkey}'
+            products = products.order_by(sortkey)
 
     if request.GET:
         if 'category' in request.GET:
@@ -19,9 +36,12 @@ def all_products(request):
             products = products.filter(category__name__in=categories)
             categories = Category.objects.filter(name__in=categories)
 
+        current_sorting = f'{sort}_{direction}'
+
     context = {
         'products': products,
         'current_categories': categories,
+        'current_sorting': current_sorting,
     }
 
     return render(request, 'products.html', context)
