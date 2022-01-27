43237f176fa2d4d7fe1ae42ec9e0ccad935e28ba
Claire Potter <clairepotter019@gmail.com>
2022-01-27 12:12:26 +0000

    Add category filtering

--- store/views.py
+++ store/views.py
@@ -1,14 +1,22 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Product
+from .models import Product, Category
 
 
 def all_products(request):
     """ A view to show all products, including sorting and search queries """
 
     products = Product.objects.all()
+    categories = None
+
+    if request.GET:
+        if 'category' in request.GET:
+            categories = request.GET['category'].split()
+            products = products.filter(category__name__in=categories)
+            categories = Category.objects.filter(name__in=categories)
 
     context = {
         'products': products,
+        'current_categories': categories,
     }
 
     return render(request, 'products.html', context)
