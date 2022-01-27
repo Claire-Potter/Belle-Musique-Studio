0b96d49df3daedcd80821c4ccf5d6418cab52d10
Claire Potter <clairepotter019@gmail.com>
2022-01-27 17:48:54 +0000

    Add sort by section to products page

--- store/views.py
+++ store/views.py
@@ -17,26 +17,27 @@ def all_products(request):
     direction = None
 
     if request.GET:
+
         if 'sort' in request.GET:
             sortkey = request.GET['sort']
             sort = sortkey
             if sortkey == 'name':
                 sortkey = 'lower_name'
                 products = products.annotate(lower_name=Lower('name'))
-
+            if sortkey == 'category':
+                sortkey = 'category__name'
             if 'direction' in request.GET:
                 direction = request.GET['direction']
                 if direction == 'desc':
                     sortkey = f'-{sortkey}'
             products = products.order_by(sortkey)
 
-    if request.GET:
         if 'category' in request.GET:
-            categories = request.GET['category'].split()
+            categories = request.GET['category'].split(',')
             products = products.filter(category__name__in=categories)
             categories = Category.objects.filter(name__in=categories)
 
-        current_sorting = f'{sort}_{direction}'
+    current_sorting = f'{sort}_{direction}'
 
     context = {
         'products': products,
