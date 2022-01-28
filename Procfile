085f2da394189579ab23885dfc8d987a23b915f0
Claire Potter <clairepotter019@gmail.com>
2022-01-28 15:23:38 +0000

    Finishing adding changes for deployment

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn belle_musique_studio.wsgi:application
--- belle_musique_studio/settings.py
+++ belle_musique_studio/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost']
+ALLOWED_HOSTS = ['belle-musique-studio.herokuapp.com', 'localhost']
 
 
 # Application definition
