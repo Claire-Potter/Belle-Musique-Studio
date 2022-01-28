cf68208ec59b5dc2198018e6d5a925eae7f58c60
Claire Potter <clairepotter019@gmail.com>
2022-01-28 15:11:13 +0000

    Add adverts for contact page and style

--- store/models.py
+++ store/models.py
@@ -23,7 +23,9 @@ class Product(models.Model):
     sku = models.CharField(max_length=254, null=True, blank=True)
     name = models.CharField(max_length=254)
     description = models.TextField()
-    price = MoneyField(max_digits=6, decimal_places=2, default_currency='GBP')
+    price = (MoneyField(max_digits=100,
+                        decimal_places=2, default='0.00',
+                        default_currency='GBP'))
     rating = (models.DecimalField
               (max_digits=6, decimal_places=2, null=True, blank=True))
     image_url = models.URLField(max_length=1024, null=True, blank=True)
