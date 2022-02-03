ea39b5b026bb3e7eb7f84066df341f565d8a4a9b
Claire Potter <clairepotter019@gmail.com>
2022-02-03 11:59:42 +0000

    Add stripe element

--- checkout/views.py
+++ checkout/views.py
@@ -18,6 +18,8 @@ def checkout(request):
         'covers': covers,
         'cover': cover,
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_0SMREd7Vdweb1MGRi8S0EycR00JVzSAs5O',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
