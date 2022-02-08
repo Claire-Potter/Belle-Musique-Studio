121b3a414927e94afe53e2830f93e46e975f9e8c
Claire Potter <clairepotter019@gmail.com>
2022-02-08 16:39:06 +0000

    Add workshops urls views and templates

--- workshops/views.py
+++ workshops/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
-# Create your views here.
+
+
+def workshops(request):
+    """ A view to show the music store """
+    covers = Cover.objects.all()
+    cover = get_object_or_404(covers, page='workshops')
+    context = { 'covers': covers,
+                'cover': cover}
+
+    return render(request, 'workshops/workshops.html', context)
