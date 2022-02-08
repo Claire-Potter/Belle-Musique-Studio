5804ba65893f2029ab7bfea9c630ad7658df2e82
Claire Potter <clairepotter019@gmail.com>
2022-02-08 16:52:50 +0000

    Add lessons view, url and template

--- belle_musique_studio/urls.py
+++ belle_musique_studio/urls.py
@@ -39,4 +39,5 @@ urlpatterns = [
     path('bag/', include('shopping_bag.urls')),
     path('checkout/', include('checkout.urls')),
     path('profiles/', include('profiles.urls')),
+    path('lessons/', include('lessons.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- lessons/views.py
+++ lessons/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from home.models import Cover
 
-# Create your views here.
+
+
+def lessons(request):
+    """ A view to show the lessons page """
+    covers = Cover.objects.all()
+    cover = get_object_or_404(covers, page='lessons')
+    context = { 'covers': covers,
+                'cover': cover}
+
+    return render(request, 'lessons/lessons.html', context)
+from django.shortcuts import render
