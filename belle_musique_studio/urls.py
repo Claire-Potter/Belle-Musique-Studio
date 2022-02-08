f8c8536f9a5532024dcb9e0ade95cd3b8d288d2e
Claire Potter <clairepotter019@gmail.com>
2022-02-08 16:42:36 +0000

    Add urls for new apps

--- belle_musique_studio/urls.py
+++ belle_musique_studio/urls.py
@@ -38,4 +38,6 @@ urlpatterns = [
     path('products/', include('store.urls')),
     path('bag/', include('shopping_bag.urls')),
     path('checkout/', include('checkout.urls')),
+    path('workshops/', include('workshops.urls')),
+    path('lessons/', include('lessons.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
