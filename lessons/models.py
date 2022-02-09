9e55b7a52db7485734fcf0e663ed994c92fc915f
Claire Potter <clairepotter019@gmail.com>
2022-02-09 22:54:03 +0000

    Create lessons.html and update view, style

--- lessons/models.py
+++ lessons/models.py
@@ -29,6 +29,8 @@ class Lesson(models.Model):
     sku = models.CharField(max_length=254, null=True, blank=True)
     name = models.CharField(max_length=254)
     description = models.TextField()
+    image_url = models.URLField(max_length=1024, null=True, blank=True)
+    image_description = models.TextField(null=True, blank=True)
 
     def __str__(self):
         return '%s' % (self.name)
--- lessons/views.py
+++ lessons/views.py
@@ -1,14 +1,35 @@
+""".git/"""
 from django.shortcuts import render, get_object_or_404
 from home.models import Cover
+from .models import Lesson, Subscription
 
 
-
-def lessons(request):
+def lessons_details(request):
     """ A view to show the lessons page """
     covers = Cover.objects.all()
     cover = get_object_or_404(covers, page='lessons')
+    piano_lessons = Lesson.objects.filter(type=1)
+    piano_lesson_image = get_object_or_404(piano_lessons,
+                                           image_description='A young girl playing the piano')
+    singing_lessons = Lesson.objects.filter(type=2)
+    singing_lesson_image = get_object_or_404(singing_lessons,
+                                             image_description='A young boy'
+                                                               ' singing into a microphone')
+    song_lessons = Lesson.objects.filter(type=3)
+    song_lesson_image = get_object_or_404(song_lessons,
+                                          image_description='A woman playing the piano and singing')
+    subscriptions = Subscription.objects.all()
+    subscription_thirty = get_object_or_404(subscriptions, name='Weekly Payment - 30 minute lesson')
+    subscription_forty_five = get_object_or_404(subscriptions, name='Weekly Payment - 45 minute lesson')
     context = { 'covers': covers,
-                'cover': cover}
+                'cover': cover,
+                'piano_lessons': piano_lessons,
+                'piano_lesson_image': piano_lesson_image,
+                'singing_lessons': singing_lessons,
+                'singing_lesson_image': singing_lesson_image,
+                'song_lessons': song_lessons,
+                'song_lesson_image': song_lesson_image,
+                'subscription_thirty': subscription_thirty,
+                'subscription_forty_five': subscription_forty_five}
 
     return render(request, 'lessons/lessons.html', context)
-from django.shortcuts import render
