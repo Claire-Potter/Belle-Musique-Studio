2869c9de77e125f6b032e715df1eedc9d4c4e7c7
Claire Potter <clairepotter019@gmail.com>
2022-02-09 18:00:50 +0000

    Create models, admin and upload data

--- lessons/admin.py
+++ lessons/admin.py
@@ -1,3 +1,34 @@
+""".git/"""
 from django.contrib import admin
+from .models import Lesson, Type, Subscription
 
-# Register your models here.
+
+class LessonAdmin(admin.ModelAdmin):
+    """.git/"""
+    list_display = (
+        'sku',
+        'name',
+        'type',
+    )
+
+    ordering = ('sku',)
+
+
+class LessonTypeAdmin(admin.ModelAdmin):
+    """.git/"""
+    list_display = (
+        'call_type',
+        'type',
+    )
+
+
+class SubscriptionAdmin(admin.ModelAdmin):
+    """.git/"""
+    list_display = (
+        'name',
+    )
+
+
+admin.site.register(Lesson, LessonAdmin)
+admin.site.register(Type, LessonTypeAdmin)
+admin.site.register(Subscription, SubscriptionAdmin)
