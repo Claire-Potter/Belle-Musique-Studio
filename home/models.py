379c767d5cfd033912f35723e252fa3708d0f967
Claire Potter <clairepotter019@gmail.com>
2022-03-03 13:37:46 +0000

    Fix comment causing build errors

--- home/models.py
+++ home/models.py
@@ -15,7 +15,7 @@ from djstripe.models import Customer, Subscription
 
 
 #class User(AbstractUser):
-    """.git/"""
+   # """.git/"""
    # customer = models.ForeignKey(Customer, null=True, blank=True, on_delete=models.SET_NULL)
    # subscription = models.ForeignKey(Subscription, null=True, blank=True,on_delete=models.SET_NULL)
 
