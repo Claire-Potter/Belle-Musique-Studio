f6c4fb023e5b3507f1505e830ea29a10b12cffd9
Claire Potter <clairepotter019@gmail.com>
2022-03-18 12:27:36 +0000

    Correct and clean up across lessons app

--- lessons/forms.py
+++ lessons/forms.py
@@ -16,10 +16,6 @@ class LessonProductForm(forms.ModelForm):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        products = Product.objects.all()
-        product_names = [(p.id, p.name ) for p in products]
-
-        self.fields['name'].choices = product_names
         self.fields['id'].widget.attrs['readonly'] = True
         for field_name, field in self.fields.items():
             field.widget.attrs['class'] = 'border-black'
@@ -31,7 +27,7 @@ class LessonPriceForm(forms.ModelForm):
     class Meta:
         """.git/"""
         model = Plan
-        fields = ('id', 'amount', 'currency', 'interval', 'nickname', 'usage_type')
+        fields = ('id', 'amount', 'currency', 'interval', 'nickname', 'usage_type', 'trial_period_days')
 
 
     def __init__(self, *args, **kwargs):
@@ -57,10 +53,23 @@ class LessonProductAddForm(forms.ModelForm):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        products = Product.objects.all()
-        product_names = [(p.id, p.name ) for p in products]
 
-        self.fields['name'].choices = product_names
+        placeholders = {
+            'name': 'The product\'s name, meant to be displayable to the customer.',
+            'description': 'A description of this object.',
+            'url': 'A URL of a publicly-accessible image address.',
+            'caption': 'The alt text for the image.',
+            'statement_descriptor': 'Appears on your customer\'s credit card statement.',
+            'unit_label': 'Billed unit title eg: lesson.',
+        }
+
+        for field in self.fields:
+            if self.fields[field].required:
+                placeholder = f'{placeholders[field]} *'
+            else:
+                placeholder = placeholders[field]
+            self.fields[field].widget.attrs['placeholder'] = placeholder
+
         for field_name, field in self.fields.items():
             field.widget.attrs['class'] = 'border-black'
 
@@ -71,11 +80,30 @@ class LessonPriceAddForm(forms.ModelForm):
     class Meta:
         """.git/"""
         model = Plan
-        fields = ('amount', 'currency', 'interval', 'nickname', 'usage_type', 'active')
+        fields = ('amount', 'currency', 'interval', 'nickname',
+        'usage_type', 'active', 'trial_period_days')
 
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+
+        placeholders = {
+            'amount': 'Amount (as decimal) to be charged on the interval specified.',
+            'currency': 'gbp',
+            'interval': 'A URL of a publicly-accessible image address.',
+            'nickname': 'The alt text for the image',
+            'usage_type': 'Appears on your customer\'s credit card statement',
+            'trial_period_days': 'Billed unit title eg: lesson',
+        }
+        for field in self.fields:
+            if field not in ('active', ):
+                if self.fields[field].required:
+                    placeholder = f'{placeholders[field]} *'
+                else:
+                    placeholder = placeholders[field]
+                self.fields[field].widget.attrs['placeholder'] = placeholder 
+        
         self.fields['active'].initial = True
+
         for field_name, field in self.fields.items():
             field.widget.attrs['class'] = 'border-black'
