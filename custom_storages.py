e52e6b14157d0495fbf23a48f4b6df69ac02aa61
Claire Potter <clairepotter019@gmail.com>
2022-03-23 16:47:14 +0000

    Add comments to custom storages

--- custom_storages.py
+++ custom_storages.py
@@ -1,10 +1,21 @@
+"""
+Belle Musique Studio custom storages configuration
+"""
 from django.conf import settings
+# The Django settings file contains all of the configuration for a web application.
 from storages.backends.s3boto3 import S3Boto3Storage
+# Amazon Simple Storage Service (S3)
 
 
 class StaticStorage(S3Boto3Storage):
+    """
+    Set static files location as Amazon S3
+    """
     location = settings.STATICFILES_LOCATION
 
 
 class MediaStorage(S3Boto3Storage):
+    """
+    Set media files location as Amazon S3
+    """
     location = settings.MEDIAFILES_LOCATION
