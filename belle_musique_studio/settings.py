93f998fe81093e7ac3875ee81f82282c1b4501b7
Claire Potter <clairepotter019@gmail.com>
2022-03-26 16:26:46 +0000

    Save changes after running validate templates

--- belle_musique_studio/settings.py
+++ belle_musique_studio/settings.py
@@ -52,6 +52,7 @@ INSTALLED_APPS = [
     'django.contrib.sites',
     'django_countries',
     'django_summernote',
+    'django_extensions',
     'djstripe',
     'allauth',
     'allauth.account',
