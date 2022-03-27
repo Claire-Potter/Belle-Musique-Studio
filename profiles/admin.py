6e4d86d1f14b7bf9e9c97bf02f2b03b9625cbc69
Claire Potter <clairepotter019@gmail.com>
2022-03-27 10:11:48 +0000

    Make changes in alignment with staff access setup

--- profiles/admin.py
+++ profiles/admin.py
@@ -20,13 +20,13 @@ class UserProfileAdmin(admin.ModelAdmin):
     """
     model = UserProfile
 
-    readonly_fields = ('user', 'default_full_name', 'default_email',
-                       'default_additional_email',
-                       'default_phone_number', 'default_street_address1',
-                       'default_street_address2',
-                       'default_town_or_city', 'default_county',
-                       'default_postcode',
-                       'default_country',)
+    fields = ('user', 'default_full_name', 'default_email',
+              'default_additional_email',
+              'default_phone_number', 'default_street_address1',
+              'default_street_address2',
+              'default_town_or_city', 'default_county',
+              'default_postcode',
+              'default_country',)
     list_display = ('user', 'default_full_name', 'default_email')
 
     def has_delete_permission(self, request, obj=None):
